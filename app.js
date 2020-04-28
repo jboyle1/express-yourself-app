59f4cdfd92ff6d4c92a3854dcd66b645da36b7c4
Jed Boyle <jedboyle01@gmail.com>
2020-04-28 18:57:53 +0100

    Use static server to serve the Express Yourself website. Also numbered comments to keep track of steps taken.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-// Import the express library
+// 001-Import the express library
 const express = require('express');
 
-// Instantiate the app
+// 002-Instantiate the app
 const app = express();
 
-// Listen to specific server
+// 003-Listen to specific server
 const PORT = process.env.PORT || 3000;
 
-// Invoke the app's '.listen()' method
+// Use static server to serve the Express Yourself website
+app.use(express.static('public'));
+
+
+// 004-Invoke the app's '.listen()' method
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
