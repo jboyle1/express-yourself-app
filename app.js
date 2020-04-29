3fe6fff8d8faeba7df0c9e3d07f3a679fdb22df6
Jed Boyle <jedboyle01@gmail.com>
2020-04-29 10:15:14 +0100

    Create a POST /expressions route handler that sends, creates and adds a new expression to the expressions array if it is a valid new expression (meaning it has an emoji and name key). It should send back the new element with a 201 status code if it is valid, and it should send a 400 status code if the object is not valid.

--- app.js
+++ app.js
@@ -39,15 +39,25 @@ app.get('/expressions/:id', (req, res, next) => {
 // 013-Open a PUT /expression/;id put route handler
 app.put('/expressions/:id', (res, req, next) => {
     const expressionIndex = getElementById(req.params.id, expressions);
-     // 014-Use req.query to update the proper element in the expressions array and llow for invalid requests using an if else conditional statement
+     // 014-Use req.query to update the proper element in the expressions array and allow for invalid requests using an if else conditional statement
     if (expressionIndex !== -1) {
         updateElement(req.params.id, req.query, expressions);
         res.send(expressions[expressionsIndex]);
     } else {
         res.status(404).send();
     }
-   
 });
+
+// 015-Create a POST /expressions route handler that sends, creates and adds a new expression to the expressions array if it is a valid new expression (meaning it has an emoji and name key). It should send back the new element with a 201 status code if it is valid, and it should send a 400 status code if the object is not valid.
+app.post('/expressions', (req, res, next) => {
+    const receivedExpression = createElement('expressions', req.query);
+    if (receivedExpression) {
+        expressions.push(receivedExpression);
+        res.status(201).send(receivedExpression);
+    } else {
+        res.status(400).send();
+    }
+})
 
 // 004-Invoke the app's '.listen()' method
 app.listen(PORT, () => {
